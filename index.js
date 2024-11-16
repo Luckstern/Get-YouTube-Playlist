e9ee898808df3eca7a9770152266ce1e9b166e5a
Luckstern <rajghole@icloud.com>
2024-11-16 02:03:47 -0500

    Wrote most of the code

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-async function getID(url) {
-    fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(url)}`, {method: "GET"})
-        .then(res => res.text())
-        .then(text => console.log(text));
-}
-
 function getLatest() {
     let channelUrl = document.getElementById('url').value;
-    let id = getID(channelUrl).substring(2);
-    
+    fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelUrl)}`, {method: "GET"})
+        .then(res => res.text())
+        .then(text => {
+            //let id = 'https://www.youtube.com/playlist?list=UULF'+text.substring(2);
+            let id = 'https://www.youtube.com/embed/videoseries?list=UULF'+text.substring(2);
+            document.getElementById("link").src = id;
+            //document.getElementById("link").innerHTML = id;
+        });
 }
 
 function getPopular() {
-    // TODO
+    let channelUrl = document.getElementById('url').value;
+    fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelUrl)}`, {method: "GET"})
+        .then(res => res.text())
+        .then(text => {
+            //let id = 'https://www.youtube.com/playlist?list=UULP'+text.substring(2);
+            let id = 'https://www.youtube.com/embed/videoseries?list=UULP'+text.substring(2);
+            document.getElementById("link").src = id;
+            //document.getElementById("link").innerHTML = id;
+        });
 }

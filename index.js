d3807ee49deb4efa923ed5bae824d97d1804282b
Luckstern <rajghole@icloud.com>
2024-11-17 03:22:29 -0500

    Added more features

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 function getLatest() {
-    let channelUrl = document.getElementById('url').value;
-    fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelUrl)}`, {method: "GET"})
-        .then(res => res.text())
-        .then(text => {
-            document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULF' + text.substring(2);
+    if (document.getElementById('channel-input').checkValidity()) {
+        document.getElementById("status").innerText = "";
+        let channelInput = document.getElementById('channel-input').value;
+        if (document.getElementById("manual").checked) {
+            document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULF' + channelInput.substring(2);
             document.getElementById("instructions").style.display = "none";
-        });
+        } else {
+            fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelInput)}`, {method: "GET"})
+                .then(res => res.text())
+                .then(text => {
+                    document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULF' + text.substring(2);
+                    document.getElementById("instructions").style.display = "none";
+                });
+        }
+    } else {
+        document.getElementById("status").innerText = "Please enter a valid input!";
+    }
 }
 
 function getPopular() {
-    let channelUrl = document.getElementById('url').value;
-    fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelUrl)}`, {method: "GET"})
-        .then(res => res.text())
-        .then(text => {
-            document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULP' + text.substring(2);
+    if (document.getElementById('channel-input').checkValidity()) {
+        document.getElementById("status").innerText = "";
+        let channelInput = document.getElementById('channel-input').value;
+        if (document.getElementById("manual").checked) {
+            document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULP' + channelInput.substring(2);
             document.getElementById("instructions").style.display = "none";
-        });
+        } else {
+            fetch(`https://yt-api.luckstern.hackclub.app/id?url=${encodeURIComponent(channelInput)}`, {method: "GET"})
+                .then(res => res.text())
+                .then(text => {
+                    document.getElementById("link").src = 'https://www.youtube.com/embed/videoseries?list=UULP' + text.substring(2);
+                    document.getElementById("instructions").style.display = "none";
+                });
+        }
+    } else {
+        document.getElementById("status").innerText = "Please enter a valid input!";
+    }
 }
 
 function editIframeBorderRadius() {
@@ -23,5 +43,17 @@ function editIframeBorderRadius() {
         document.getElementById("link").style.borderRadius = "0";
     } else {
         document.getElementById("link").style.borderRadius = "0 12px 12px 0";
+    }
+}
+
+function manualInput(e) {
+    if (e.target.checked) {
+        document.getElementById("channel-label").innerText = "Channel ID";
+        document.getElementById("channel-input").placeholder = "UCBR8-60-B28hp2BmDPdntcQ";
+        document.getElementById("channel-input").pattern = "([A-Za-z0-9]*(-[A-Za-z0-9]*)*)";
+    } else {
+        document.getElementById("channel-label").innerText = "Channel Link";
+        document.getElementById("channel-input").placeholder = "https://www.youtube.com/@YouTube";
+        document.getElementById("channel-input").pattern = "(https://)?(www.)?youtube.com/@[A-Za-z]+";
     }
 }
